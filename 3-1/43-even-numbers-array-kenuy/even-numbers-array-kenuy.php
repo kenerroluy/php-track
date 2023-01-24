6d6385889a35028b0ed66afcefdf2e1a26927d94
Ken Uy <kenerroluy@gmail.com>
2023-01-24 19:04:31 +0800

    add 43

---
+++ 3-1/43-even-numbers-array-kenuy/even-numbers-array-kenuy.php
@@ -0,0 +1,11 @@
+<?php 
+    $even_array = array();
+
+    for($i = 0; $i <= 10000; $i++){
+        if($i % 2 == 0 ){
+            array_push($even_array, $i);
+        }
+    }
+    
+    var_dump($even_array);
+?>
